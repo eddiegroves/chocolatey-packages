d5f1e2c744b8d89bee1eee40e7cb17169ab8213d
Eddie Groves <eddiegroves@gmail.com>
2015-06-07 07:59:03 +1000

    Fix download and icon locations

--- Cntlm/tools/chocolateyInstall.ps1
+++ Cntlm/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 ﻿$packageName = 'Cntlm'
-$url = 'http://aarnet.dl.sourceforge.net/project/cntlm/cntlm/cntlm 0.92.3/cntlm-0.92.3-setup.exe' # download url
+$url = 'http://sourceforge.net/projects/cntlm/files/cntlm/cntlm%200.92.3/cntlm-0.92.3-setup.exe/download'
 
-Install-ChocolateyPackage "$packageName" 'EXE' '/VERYSILENT' "$url"
+Install-ChocolateyPackage "$packageName" 'EXE' '/VERYSILENT' "$url" -checksum 9d8ee87cbf8a8cbae837fe705468ca2e
